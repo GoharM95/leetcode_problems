de22d9afe5557a3200a5ac5525328588fd61c6ea
Gohar <gohar.mamunts@gmail.com>
2022-04-13 19:38:07 +0400

    longestCommonPrefix-isValid-removeDuplicates

--- index.js
+++ index.js
@@ -66,3 +66,81 @@ function romanToInt(roman) {
 
 console.log(romanToInt("XCIV"));
 console.log(romanToInt("LVIII"));
+
+// 14
+function longestCommonPrefix(strsArr) {
+  let longestPrefix = "";
+  if (strsArr.length === 0) {
+    return longestPrefix;
+  }
+
+  let comparisonWord = strsArr[0];
+  let comparisonIndex = 0;
+
+  for (let comparisonLetter of comparisonWord) {
+    for (let i = 1; i < strsArr.length; i++) {
+      let currentWord = strsArr[i];
+      let currentLetter = currentWord[comparisonIndex];
+
+      if (
+        comparisonIndex > currentWord.length ||
+        comparisonLetter !== currentLetter
+      ) {
+        return longestPrefix;
+      }
+    }
+    comparisonIndex++;
+    longestPrefix += comparisonLetter;
+  }
+
+  return longestPrefix;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+
+// 20
+
+const pairBrs = {
+  "(": ")",
+  "{": "}",
+  "[": "]",
+};
+
+function isValid(brStr) {
+  if (brStr.length < 2) {
+    return false;
+  }
+
+  let stk = [];
+  let brStrArr = brStr.split("");
+  for (let br of brStrArr) {
+    if (pairBrs[br]) {
+      stk.push(br);
+    } else {
+      const checkBr = stk.pop();
+      if (pairBrs[checkBr] !== br) {
+        return false;
+      }
+    }
+  }
+  if (stk.length > 0) {
+    return false;
+  }
+  return true;
+}
+
+console.log(isValid("()[]{}"));
+
+// 26
+function removeDuplicates(arr) {
+  let pointer1 = 0;
+  for (let pointer2 = 1; pointer2 < arr.length; pointer2++) {
+    if (arr[pointer1] !== arr[pointer2]) {
+      pointer1++;
+      arr[pointer1] = arr[pointer2];
+    }
+  }
+  return pointer1 + 1;
+}
+
+console.log(removeDuplicates([1, 1, 2]));
