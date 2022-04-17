1d63d8b1ca24c0d3ea9b0e2755e36212f7b97631
Gohar <gohar.mamunts@gmail.com>
2022-04-17 21:51:53 +0400

    countSegments

---
+++ problems/countSegments.js
@@ -0,0 +1,25 @@
+// Given a string s, return the number of segments in the string.
+// A segment is defined to be a contiguous sequence of non-space characters.
+
+// Input: s = "Hello, my name is John"
+// Output: 5
+// Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+
+// 434
+function countSegments(str) {
+  let count = 0;
+  let i = 0;
+
+  while (i < str.length) {
+    if (str[i] !== " ") {
+      count++;
+      while (i < str.length && str[i] !== " ") {
+        i++;
+      }
+    }
+    i++;
+  }
+  return count;
+}
+
+console.log(countSegments("Hello, my name is John"));
