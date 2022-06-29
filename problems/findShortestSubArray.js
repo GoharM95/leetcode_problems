d5ecd983447ec3a8d5d1660ffcf35e9ec40fd83c
Gohar <gohar.mamunts@gmail.com>
2022-06-29 22:32:29 +0400

    adds more efficient solution

--- problems/findShortestSubArray.js
+++ problems/findShortestSubArray.js
@@ -4,6 +4,8 @@
 // (contiguous) subarray of nums, that has the same degree as nums.
 
 // 697. Degree of an Array
+
+// 1
 function findShortestSubArray(arr) {
   const elemsCountObj = {};
 
@@ -25,6 +27,41 @@ function findShortestSubArray(arr) {
   }
 
   return minLengthArr;
+}
+
+// 2
+function findShortestSubArray(arr) {
+  const nestedObj = {};
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    if (!nestedObj[[elem]]) {
+      nestedObj[elem] = {
+        count: 1,
+        firstIndex: arr.indexOf(elem),
+        lastIndex: arr.lastIndexOf(elem),
+      };
+    } else {
+      nestedObj[[elem]].count++;
+    }
+  }
+
+  const arrDegree = Math.max(
+    ...Object.values(nestedObj).map((obj) => obj.count)
+  );
+
+  const objectsArr = Object.values(nestedObj);
+  let smallestSubArrayLength = arr.length;
+
+  for (let i = 0; i < objectsArr.length; i++) {
+    if (objectsArr[i].count === arrDegree) {
+      smallestSubArrayLength = Math.min(
+        smallestSubArrayLength,
+        objectsArr[i].lastIndex - objectsArr[i].firstIndex + 1
+      );
+    }
+  }
+
+  return smallestSubArrayLength;
 }
 
 console.log(findShortestSubArray([1, 2, 2, 3, 1]));
