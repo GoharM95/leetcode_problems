ffa49fdf8d4dd9e5514ce403aad2b96df951c3a7
Gohar <gohar.mamunts@gmail.com>
2022-07-22 18:12:28 +0400

    adds 3rd solution

--- problems/findShortestSubArray.js
+++ problems/findShortestSubArray.js
@@ -66,3 +66,39 @@ function findShortestSubArray(arr) {
 
 console.log(findShortestSubArray([1, 2, 2, 3, 1]));
 console.log(findShortestSubArray([1, 2, 2, 3, 1, 4, 2]));
+
+// 3
+function findShortestSubArray(arr) {
+  let arrDegree = 0;
+  let smallestSubArrayLength = arr.length;
+
+  const nestedObj = {};
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    if (!nestedObj[[elem]]) {
+      nestedObj[elem] = {
+        count: 1,
+        firstIndex: i,
+        lastIndex: i,
+      };
+    } else {
+      nestedObj[elem].lastIndex = i;
+      nestedObj[elem].count++;
+    }
+
+    const currSubArrLength =
+      nestedObj[elem].lastIndex - nestedObj[elem].firstIndex + 1;
+
+    if (arrDegree < nestedObj[elem].count) {
+      arrDegree = nestedObj[elem].count;
+      smallestSubArrayLength = currSubArrLength;
+    } else if (arrDegree === nestedObj[elem].count) {
+      smallestSubArrayLength = Math.min(
+        smallestSubArrayLength,
+        currSubArrLength
+      );
+    }
+  }
+
+  return smallestSubArrayLength;
+}
