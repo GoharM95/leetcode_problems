59e7976088de1d5fb168ac9db42502a2a75b6c73
Gohar <gohar.mamunts@gmail.com>
2022-07-24 10:19:03 +0400

    isToeplitzMatrix

---
+++ isToeplitzMatrix/index.js
@@ -0,0 +1,20 @@
+// 766. Toeplitz Matrix
+function isToeplitzMatrix(matrix) {
+  for (let i = 0; i < matrix.length - 1; i++) {
+    for (let j = 0; j < matrix[i].length - 1; j++) {
+      if (matrix[i][j] !== matrix[i + 1][j + 1]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+console.log(
+  isToeplitzMatrix([
+    [1, 2, 3, 4],
+    [5, 1, 2, 3],
+    [9, 5, 1, 2],
+  ])
+);
